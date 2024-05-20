17fc61f91866374cbb28be6bd67b982530effc97
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-21 01:06:25 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.kdlxq0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.da6po2r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, } });
